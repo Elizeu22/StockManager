8f9aec31dc8c9136c97fc4696e56b6efdc969332
Elizeu Alves <elizeu.alves@01tec.com.br>
2026-08-14 16:23:14 -0300

    Adicionado requisicao post e alterado os inputs de Cadastrar Estoque

--- src/Components/Formulario-CadastrarEstoque/CadastrarEstoque.jsx
+++ src/Components/Formulario-CadastrarEstoque/CadastrarEstoque.jsx
@@ -1,7 +1,60 @@
 
+import { UNSAFE_getPatchRoutesOnNavigationFunction } from 'react-router-dom';
 import './CadastrarEstoque.css'
+import { useState } from 'react'
+
 
 function CadastrarEstoque() {
+
+    const [nome, setNome] = useState('');
+    const [descricao, setDescricao] = useState('');
+    const [preco, setPreco] = useState('');
+    const [quantidadeEstoque, setQuantidadeEstoque] = useState('');
+
+
+
+    const gravacaoDados = async () => {
+        const novoProduto = {
+            nome: nome,
+            descricao: descricao,
+            preco: Number(preco),
+            quantidadeEstoque: Number(quantidadeEstoque)
+        };
+
+        try {
+            const response = await fetch('https://localhost:7154/produtos', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(novoProduto)
+            });
+
+            if (!response.ok) {
+                throw new Error('Erro ao cadastrar o produto');
+            }
+
+            const data = await response.json();
+
+            console.log('Produto cadastrado:', data);
+
+            alert('Produto cadastrado com sucesso!');
+
+            //Limpar formulario
+
+            setProduto('');
+            setDescricao('');
+            setPreco('');
+            setQuantidade('');
+        }
+        catch (error) {
+            console.error(error);
+            alert('Erro ao cadastrar produto')
+        }
+    };
+
+
+
     return (
         <form className='containerformulario'>
 
@@ -10,23 +63,23 @@ function CadastrarEstoque() {
             </div>
 
             <div className="containerCadastrarEstoque">
-                <input type="text" className="form-control" placeholder="&#128709; Produto" />
+                <input type="text" className="form-control" placeholder="&#128709; Produto" value={nome} onChange={(e) => setNome(e.target.value)} />
             </div>
 
             <div className="containerCadastrarEstoque">
-                <textarea className="form-control" placeholder="&#128709; Descrição do produto" rows={4}></textarea>
+                <textarea className="form-control" placeholder="&#128709; Descrição do produto" rows={4} value={descricao} onChange={(e) => setDescricao(e.target.value)}></textarea>
             </div>
 
             <div className="containerCadastrarEstoque">
-                <input type="text" className="form-control" placeholder="&#128178; Preco produto" />
+                <input type="text" className="form-control" placeholder="&#128178; Preco produto" value={preco} onChange={(e) => setPreco(e.target.value)} />
             </div>
 
             <div className="containerCadastrarEstoque">
-                <input type="text" className="form-control" placeholder="&#128471; Quantidade produtos" />
+                <input type="text" className="form-control" placeholder="&#128471; Quantidade produtos" value={quantidadeEstoque} onChange={(e) => setQuantidadeEstoque(e.target.value)} />
             </div>
 
             <div className="containerBotoes">
-                <button type="button" className="btn btn-primary">&#128190; Cadastrar</button>
+                <button type="button" className="btn btn-primary" onClick={gravacaoDados}>&#128190; Cadastrar</button>
                 <button type="button" className="btn btn-primary">&#10060; Fechar</button>
             </div>
         </form>
